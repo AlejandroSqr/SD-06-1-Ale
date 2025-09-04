a93ad765299a9a13ba70d88a54103f10d3955f73
AlejandroDel <alejandrohdzd@proton.me>
2025-09-04 16:10:16 -0600

    Update task2.js

--- task2.js
+++ task2.js
@@ -5,7 +5,9 @@ function Mail(subj, msg) {
   
   // Type your code below this line!
   
-  const newMail = new Mail(process.argv[2],process.argv[3] )
-  // Type your code above this line!
-  
-  console.log(newMail.subject + ": " + newMail.message)
+ 
+const newMail = new Mail(process.argv[2],process.argv[3] )
+
+// Type your code above this line!
+//Aqui ya no es por consola, aqui tenemos que modificar los parametros, process "2" siendo "hello" y Process 3 "world"
+console.log(newMail.subject + ": " + newMail.message)
