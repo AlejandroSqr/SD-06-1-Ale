18d837a12130054d5d0c35c057c95b6d509a3881
AlejandroDel <alejandrohdzd@proton.me>
2025-09-04 16:53:17 -0600

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,9 +1,21 @@
 // Type your code below this line!
 
-
-
-// Type your code above this line!
-
 const travel = new Journey(from, to)
 
 console.log("Booking a taxi from " + travel.start + " to " + travel.end + ".")
+
+// Type your code above this line!
+
+
+
+/* Codigo Personal Prueba
+
+function travel(start, end){
+    this.start = start;
+    this.end = end;
+}
+const Journey = new travel ("from", "to");
+
+console.log("Booking a taxi from " + travel.start + " to " + travel.end + ".")
+
+*7
